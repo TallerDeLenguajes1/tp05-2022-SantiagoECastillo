09cba10a4e5a1c97170890c28a543a91cf500381
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2022-05-23 23:53:51 -0300

    Punto 1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,9 +1,21 @@
-﻿
+﻿using System;
 
-Console.WriteLine("Hola Mundo");
-
-Console.WriteLine("Probamos el push");
-
-Console.WriteLine("Prueba de push por gitlens");
-
-
+int multiplicador = 1, resto;
+int numeroInvertido = 0;
+Console.WriteLine("Escriba el numero (POSTIVO) a invertir: ");
+int numero = int.Parse(Console.ReadLine());
+if(numero > 0){
+    while(numero > 0){
+        resto = numero % 10;
+        if(multiplicador == 1){
+            numeroInvertido = resto;
+            multiplicador = 10; 
+        }else{
+            numeroInvertido = (numeroInvertido * multiplicador) + resto;
+        }
+        numero = numero / 10;
+    }
+    Console.WriteLine("El numero invertido es: " + numeroInvertido);
+}else{
+    Console.WriteLine("ERROR, el numero no es positivo");
+}
