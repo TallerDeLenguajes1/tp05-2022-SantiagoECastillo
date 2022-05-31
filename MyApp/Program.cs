70ca916b6c9d3b8186afc1ac79403976bb71ae11
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2022-05-31 12:12:49 -0300

    seno y coseno

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
-int numero;
+double numero;
 
 Console.WriteLine("Ingrese un numero");
-numero = Convert.ToInt32(Console.ReadLine());
+numero = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("El numero en valor absoluto es: " + Math.Abs(numero));
 Console.WriteLine("El cuadrado del numero es: " + Math.Pow(numero,2));
 Console.WriteLine("La raiz cuadrada del numero es: " + Math.Sqrt(Math.Abs(numero)));//hago el numero positivo para la raiz
+Console.WriteLine("El coseno del numero es: " + Math.Cos(numero));
+Console.WriteLine("El seno del numero es: " + Math.Sin(numero));
+
 
 
 
