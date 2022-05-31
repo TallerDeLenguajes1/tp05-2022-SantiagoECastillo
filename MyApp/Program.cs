8dac4b031d059d52d3a917a737ecd6a166ad1a04
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2022-05-31 11:53:43 -0300

    Correccion valor absoluto con math

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -3,17 +3,8 @@ int numero;
 
 Console.WriteLine("Ingrese un numero (valor aboluto)");
 numero = Convert.ToInt32(Console.ReadLine());
-numero = valorAbsoluto(numero);
-Console.WriteLine("El numero en valor absoluto es: " + numero);
+Console.WriteLine("El numero en valor absoluto es: " + Math.Abs(numero));
 
 
-int valorAbsoluto(int numero){
-    if(numero < 0){
-        numero = numero * (-1);
-        return numero;
-    }else{
-        return numero;
-    }
-}
 
  
