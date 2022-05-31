bc15039021acc296d8bc70a370693ad3b3f4a0a7
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2022-05-31 11:47:29 -0300

    Funcion para valor absoluto

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,21 +1,19 @@
 ﻿using System;
+int numero;
 
-int multiplicador = 1, resto;
-int numeroInvertido = 0;
-Console.WriteLine("Escriba el numero (POSTIVO) a invertir: ");
-int numero = int.Parse(Console.ReadLine());
-if(numero > 0){
-    while(numero > 0){
-        resto = numero % 10;
-        if(multiplicador == 1){
-            numeroInvertido = resto;
-            multiplicador = 10; 
-        }else{
-            numeroInvertido = (numeroInvertido * multiplicador) + resto;
-        }
-        numero = numero / 10;
+Console.WriteLine("Ingrese un numero (valor aboluto)");
+numero = Convert.ToInt32(Console.ReadLine());
+numero = valorAbsoluto(numero);
+Console.WriteLine("El numero en valor absoluto es: " + numero);
+
+
+int valorAbsoluto(int numero){
+    if(numero < 0){
+        numero = numero * (-1);
+        return numero;
+    }else{
+        return numero;
     }
-    Console.WriteLine("El numero invertido es: " + numeroInvertido);
-}else{
-    Console.WriteLine("ERROR, el numero no es positivo");
 }
+
+ 
