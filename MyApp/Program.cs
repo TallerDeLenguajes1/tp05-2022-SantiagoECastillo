235b01d1373ca1f86692e1fd6f0fb7e486ce888b
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2022-05-31 14:26:39 -0300

    Parte entera

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 double numero;
 
+
 Console.WriteLine("Ingrese un numero");
 numero = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("El numero en valor absoluto es: " + Math.Abs(numero));
@@ -8,6 +9,8 @@ Console.WriteLine("El cuadrado del numero es: " + Math.Pow(numero,2));
 Console.WriteLine("La raiz cuadrada del numero es: " + Math.Sqrt(Math.Abs(numero)));//hago el numero positivo para la raiz
 Console.WriteLine("El coseno del numero es: " + Math.Cos(numero));
 Console.WriteLine("El seno del numero es: " + Math.Sin(numero));
+Console.WriteLine("La parte entera del numero es: " + Math.Truncate(numero));
+
 
 
 
