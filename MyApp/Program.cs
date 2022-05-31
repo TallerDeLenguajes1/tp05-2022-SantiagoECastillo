4c5cdf85cb7a868fce1e052fc2e459b213a5238c
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2022-05-31 12:04:15 -0300

    potencia y raiz cuadrada

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 int numero;
 
-Console.WriteLine("Ingrese un numero (valor aboluto)");
+Console.WriteLine("Ingrese un numero");
 numero = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("El numero en valor absoluto es: " + Math.Abs(numero));
+Console.WriteLine("El cuadrado del numero es: " + Math.Pow(numero,2));
+Console.WriteLine("La raiz cuadrada del numero es: " + Math.Sqrt(Math.Abs(numero)));//hago el numero positivo para la raiz
+
+
+
 
 
 
