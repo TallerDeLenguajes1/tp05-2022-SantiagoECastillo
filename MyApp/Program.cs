16a263cac4c6114dbbeb79f3d8d4c4ffbed5e811
SantiagoECastillo <castillosantiagoe1998@gmail.com>
2022-05-31 11:27:04 -0300

    Calculadora V1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,21 +1,79 @@
 ﻿using System;
 
-int multiplicador = 1, resto;
-int numeroInvertido = 0;
-Console.WriteLine("Escriba el numero (POSTIVO) a invertir: ");
-int numero = int.Parse(Console.ReadLine());
-if(numero > 0){
-    while(numero > 0){
-        resto = numero % 10;
-        if(multiplicador == 1){
-            numeroInvertido = resto;
-            multiplicador = 10; 
-        }else{
-            numeroInvertido = (numeroInvertido * multiplicador) + resto;
-        }
-        numero = numero / 10;
+int opcion = 0;
+int numero1, numero2, resultado;
+while(opcion == 0){
+
+    Console.WriteLine("---------MENU-------");
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+    Console.WriteLine("Elija una opcion: ");
+    opcion = Convert.ToInt32(Console.ReadLine());
+
+    
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("SUMA");
+            Console.WriteLine("Ingrese un numero: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            resultado = numero1 + numero2;
+            Console.WriteLine("El resultado de la suma es: " + resultado);
+            opcion = siguienteOperacion();
+            break;
+
+        case 2:
+            Console.WriteLine("RESTA");
+            Console.WriteLine("Ingrese un numero: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            resultado = numero1 - numero2;
+            Console.WriteLine("El resultado de la resta es: " + resultado);
+            opcion = siguienteOperacion();
+            break;
+
+        case 3:
+            Console.WriteLine("MULTIPLICACION");
+            Console.WriteLine("Ingrese un numero: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            resultado = numero1 * numero2;
+            Console.WriteLine("El resultado de la multiplicacion es: " + resultado);
+            opcion = siguienteOperacion();
+            break;
+
+        case 4:
+            Console.WriteLine("DIVISION");
+            Console.WriteLine("Ingrese un numero: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+            resultado = numero1 / numero2;
+            Console.WriteLine("El resultado de la division es: " + resultado);
+            opcion = siguienteOperacion();
+            break;
+        default:
+            Console.WriteLine("La opcion elegida no es valida");
+            break;
     }
-    Console.WriteLine("El numero invertido es: " + numeroInvertido);
-}else{
-    Console.WriteLine("ERROR, el numero no es positivo");
+
+    Console.WriteLine("Fin del programa");
+}
+
+
+int siguienteOperacion(){
+    Console.WriteLine("Desea realizar otra operacion ? s/n");//suponiendo que se ingresa solo s(si) o n(no)
+    var respuesta = Console.ReadLine();
+    if(respuesta == "n"){
+        return 1;
+    }else{
+        return 0;
+    }
+
 }
